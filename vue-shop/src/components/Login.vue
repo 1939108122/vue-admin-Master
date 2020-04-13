9fcb317c037453163cf40238a4c7bfba45e3a51f
1939108122 <1939108122@qq.com>
2020-04-13 23:22:20 +0800

    渲染login组件

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -1,15 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    Login
   </div>
 </template>
-
 <script>
 export default {
-  name: 'app'
 }
 </script>
-
-<style>
+<style lang="less" scoped>
 
 </style>
