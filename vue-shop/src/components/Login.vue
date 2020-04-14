dd5e3b730b5654091b1a6c9f2d639b8b1acc03b6
1939108122 <1939108122@qq.com>
2020-04-14 22:23:55 +0800

    绘制默认头像

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
     </div>
   </div>
 </template>
@@ -22,5 +25,23 @@ export default {
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+}
+.avatar_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
 }
 </style>
