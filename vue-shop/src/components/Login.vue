d014cb1948aa794e6850c78ec37bcb6f5d7b838a
1939108122 <1939108122@qq.com>
2020-04-14 22:13:38 +0800

    设置登录盒子

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Login
+  <div class="login_container">
+    <div class="login_box">
+    </div>
   </div>
 </template>
 <script>
@@ -8,5 +9,18 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+.login_container {
+  background-color:#2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
