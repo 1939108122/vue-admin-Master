035cef06c120d4be709178a5c26b3723da6b7d12
1939108122 <1939108122@qq.com>
2020-04-16 23:12:42 +0800

    主页布局

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右边主题 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -14,5 +22,16 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
