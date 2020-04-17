8ff1d634cf56d55118d6d92a31a4562157567040
1939108122 <1939108122@qq.com>
2020-04-17 23:18:01 +0800

    制作导航菜单

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -1,11 +1,37 @@
 <template>
   <el-container class="home-container">
     <!-- 头部区域 -->
-    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button></el-header>
     <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+        background-color="#333744"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <!-- 右边主题 -->
       <el-main>Main</el-main>
     </el-container>
@@ -27,6 +53,19 @@ export default {
 }
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
--- vue-shop/src/plugins/element.js
+++ vue-shop/src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Input, Form, FormItem, Message, Container, Header, Aside, Main } from 'element-ui'
+import { Button, Input, Form, FormItem, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -8,4 +8,8 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
 Vue.prototype.$message = Message
